fc5c7ce87a391f4b0c27c99311a2276e5c998f3a
Paul Scott <paul@tailscale.com>
2024-03-04 19:51:35 +0000

    cmd/tailscale/cli: tidy unnused method
    Signed-off-by: Paul Scott <paul@tailscale.com>

--- cmd/tailscale/cli/file.go
+++ cmd/tailscale/cli/file.go
@@ -411,10 +411,6 @@ func (v *onConflict) Set(s string) error {
 	return nil
 }
 
-func (v *onConflict) Words() []string {
-	return []string{"skip", "overwrite", "rename"}
-}
-
 var fileGetCmd = &ffcli.Command{
 	Name:       "get",
 	ShortUsage: "file get [--wait] [--verbose] [--conflict=(skip|overwrite|rename)] <target-directory>",
